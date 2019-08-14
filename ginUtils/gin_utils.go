6e1be340872c4aa4191929185d3e2d5837716086
Zongmin Lei <leizongmin@gmail.com>
2019-08-14 14:38:20 +0800

    add ginUtils

---
+++ ginUtils/gin_utils.go
@@ -0,0 +1,54 @@
+package ginUtils
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type H = gin.H
+type Context = gin.Context
+type Engine = gin.Engine
+
+func Default() *gin.Engine {
+	return gin.Default()
+}
+
+func New() *gin.Engine {
+	return gin.New()
+}
+
+// 自定义响应处理函数
+var ResponseHandler func(c *Context, data interface{}, err error)
+
+func init() {
+	ResponseHandler = func(c *Context, data interface{}, err error) {
+		if err != nil {
+			c.JSON(500, H{"ok": false, "error": err.Error()})
+		} else {
+			c.JSON(200, H{"ok": true, "data": data})
+		}
+	}
+}
+
+// 响应失败结果
+func ResponseError(c *Context, err error) {
+	ResponseHandler(c, nil, err)
+}
+
+// 响应成功结果
+func ResponseOk(c *Context, data interface{}) {
+	ResponseHandler(c, data, nil)
+}
+
+// 抽象服务处理接口
+func ServiceHandler(parseParams func(c *Context) []interface{}, serviceHandler func(args ...interface{}) (result interface{}, err error)) func(c *Context) {
+	return func(c *Context) {
+		result, err := serviceHandler(parseParams(c)...)
+		if err != nil {
+			ResponseError(c, err)
+		} else {
+			ResponseOk(c, result)
+		}
+	}
+}
+
+type Arg = interface{}
--- go.mod
+++ go.mod
@@ -5,14 +5,13 @@ go 1.12
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gin-gonic/gin v1.4.0
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-yaml/yaml v2.1.0+incompatible
-	github.com/jinzhu/copier v0.0.0-20190625015134-976e0346caa8
 	github.com/jmoiron/sqlx v1.2.0
 	github.com/json-iterator/go v1.1.7
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/stretchr/testify v1.3.0
 	google.golang.org/appengine v1.6.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
