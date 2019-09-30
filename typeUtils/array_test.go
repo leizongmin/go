3228de0c7b841c5e54dd829060d1f0d719a8ec60
Zongmin Lei <leizongmin@gmail.com>
2019-09-30 18:09:35 +0800

    feat(typeUtils): 增加更多单元测试

--- typeUtils/array_test.go
+++ typeUtils/array_test.go
@@ -1,6 +1,7 @@
 package typeUtils
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,6 +11,7 @@ func TestToBoolArray(t *testing.T) {
 	ret := ToBoolArray([]interface{}{true, false}, nil)
 	assert.True(t, ret[0])
 	assert.False(t, ret[1])
+	assert.Equal(t, []bool{true, false}, ret)
 }
 
 func TestToInterfaceArray(t *testing.T) {
@@ -19,4 +21,21 @@ func TestToInterfaceArray(t *testing.T) {
 	assert.Equal(t, false, ret[1])
 	assert.Equal(t, 123, ret[2])
 	assert.Equal(t, "ok", ret[3])
+}
+
+func TestToStringArray(t *testing.T) {
+	ret := ToStringArray([]interface{}{"A", "B"}, nil)
+	assert.NotNil(t, ret)
+	assert.Equal(t, "A", ret[0])
+	assert.Equal(t, "B", ret[1])
+	assert.Equal(t, []string{"A", "B"}, ret)
+}
+
+func TestToStringArray2(t *testing.T) {
+	ret := ToStringArray([]interface{}{interface{}("A"), interface{}("B")}, nil)
+	assert.NotNil(t, ret)
+	assert.Equal(t, "A", ret[0])
+	assert.Equal(t, "B", ret[1])
+	assert.Equal(t, []string{"A", "B"}, ret)
+	fmt.Printf("%+v\n", ret)
 }
