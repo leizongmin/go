165592f6f390c1a75626abc37545f23b66c1ffe7
Zongmin Lei <leizongmin@gmail.com>
2019-11-01 16:21:28 +0800

    feat(typeutil): Any 增加更多类型转换方法

--- typeutil/any.go
+++ typeutil/any.go
@@ -3,6 +3,7 @@ package typeutil
 import (
 	"fmt"
 	"reflect"
+	"time"
 )
 
 type AnyType struct {
@@ -15,7 +16,7 @@ func Any(v interface{}) AnyType {
 }
 
 // 获取interface值
-func (a AnyType) Interface() interface{} {
+func (a AnyType) Value() interface{} {
 	return a.v
 }
 
@@ -73,4 +74,340 @@ func (a AnyType) MustGet(key interface{}) AnyType {
 		panic(fmt.Sprintf(`index or key "%s" does not in value %+v`, key, a))
 	}
 	return ret
+}
+
+func (a AnyType) ToBool(defaultValue bool) bool {
+	if v, ok := a.v.(bool); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToBool() bool {
+	if v, ok := a.v.(bool); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to bool", a.v))
+	}
+}
+
+func (a AnyType) ToInt(defaultValue int) int {
+	if v, ok := a.v.(int); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToInt() int {
+	if v, ok := a.v.(int); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to int", a.v))
+	}
+}
+
+func (a AnyType) MustToInt8() int8 {
+	if v, ok := a.v.(int8); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to int8", a.v))
+	}
+}
+
+func (a AnyType) ToInt8(defaultValue int8) int8 {
+	if v, ok := a.v.(int8); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) ToInt16(defaultValue int16) int16 {
+	if v, ok := a.v.(int16); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToInt16() int16 {
+	if v, ok := a.v.(int16); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to int16", a.v))
+	}
+}
+
+func (a AnyType) ToInt32(defaultValue int32) int32 {
+	if v, ok := a.v.(int32); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToInt32() int32 {
+	if v, ok := a.v.(int32); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to int32", a.v))
+	}
+}
+
+func (a AnyType) ToInt64(defaultValue int64) int64 {
+	if v, ok := a.v.(int64); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToInt64() int64 {
+	if v, ok := a.v.(int64); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to int64", a.v))
+	}
+}
+
+func (a AnyType) ToUint(defaultValue uint) uint {
+	if v, ok := a.v.(uint); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToUint() uint {
+	if v, ok := a.v.(uint); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to uint", a.v))
+	}
+}
+
+func (a AnyType) ToUint8(defaultValue uint8) uint8 {
+	if v, ok := a.v.(uint8); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToUint8() uint8 {
+	if v, ok := a.v.(uint8); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to uint8", a.v))
+	}
+}
+
+func (a AnyType) ToUint16(defaultValue uint16) uint16 {
+	if v, ok := a.v.(uint16); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToUint16() uint16 {
+	if v, ok := a.v.(uint16); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to uint16", a.v))
+	}
+}
+
+func (a AnyType) ToUint32(defaultValue uint32) uint32 {
+	if v, ok := a.v.(uint32); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToUint32() uint32 {
+	if v, ok := a.v.(uint32); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to uint32", a.v))
+	}
+}
+
+func (a AnyType) ToUint64(defaultValue uint64) uint64 {
+	if v, ok := a.v.(uint64); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToUint64() uint64 {
+	if v, ok := a.v.(uint64); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to uint64", a.v))
+	}
+}
+
+func (a AnyType) ToFloat32(defaultValue float32) float32 {
+	if v, ok := a.v.(float32); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToFloat32() float32 {
+	if v, ok := a.v.(float32); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to float32", a.v))
+	}
+}
+
+func (a AnyType) ToFloat64(defaultValue float64) float64 {
+	if v, ok := a.v.(float64); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToFloat64() float64 {
+	if v, ok := a.v.(float64); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to float64", a.v))
+	}
+}
+
+func (a AnyType) ToString(defaultValue string) string {
+	if v, ok := a.v.(string); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToString() string {
+	if v, ok := a.v.(string); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to string", a.v))
+	}
+}
+
+func (a AnyType) ToMap(defaultValue map[string]interface{}) map[string]interface{} {
+	if v, ok := a.v.(map[string]interface{}); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToMap() map[string]interface{} {
+	if v, ok := a.v.(map[string]interface{}); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to map[string]interface{}", a.v))
+	}
+}
+
+func (a AnyType) ToTime(defaultValue time.Time) time.Time {
+	if v, ok := a.v.(time.Time); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToTime() time.Time {
+	if v, ok := a.v.(time.Time); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to time.Time", a.v))
+	}
+}
+
+func (a AnyType) ToComplex64(defaultValue complex64) complex64 {
+	if v, ok := a.v.(complex64); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToComplex64() complex64 {
+	if v, ok := a.v.(complex64); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to complex64", a.v))
+	}
+}
+
+func (a AnyType) ToComplex128(defaultValue complex128) complex128 {
+	if v, ok := a.v.(complex128); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToComplex128() complex128 {
+	if v, ok := a.v.(complex128); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to complex128", a.v))
+	}
+}
+
+func (a AnyType) ToByte(defaultValue byte) byte {
+	if v, ok := a.v.(byte); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToByte() byte {
+	if v, ok := a.v.(byte); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to byte", a.v))
+	}
+}
+
+func (a AnyType) ToRune(defaultValue rune) rune {
+	if v, ok := a.v.(rune); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToRune() rune {
+	if v, ok := a.v.(rune); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to rune", a.v))
+	}
+}
+
+func (a AnyType) ToUintptr(defaultValue uintptr) uintptr {
+	if v, ok := a.v.(uintptr); ok {
+		return v
+	} else {
+		return defaultValue
+	}
+}
+
+func (a AnyType) MustToUintptr() uintptr {
+	if v, ok := a.v.(uintptr); ok {
+		return v
+	} else {
+		panic(fmt.Sprintf("failed to cast %+v to uintptr", a.v))
+	}
 }
--- typeutil/any_test.go
+++ typeutil/any_test.go
@@ -3,6 +3,8 @@ package typeutil
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAny(t *testing.T) {
@@ -10,6 +12,14 @@ func TestAny(t *testing.T) {
 	fmt.Println(a.Get(1))
 	fmt.Println(a.Get("a"))
 	fmt.Println(a.Get("b"))
+	{
+		v, ok := a.Get("a")
+		assert.True(t, ok)
+		assert.Equal(t, 123, v.MustToInt())
+		v2, ok := a.Get("b")
+		assert.False(t, ok)
+		assert.Nil(t, v2.Value())
+	}
 
 	b := Any([2]string{"a", "b"})
 	fmt.Println(b.Get(1))
