4bb07f1534cb98ffb322fa290d3284b40623da51
Zongmin Lei <leizongmin@gmail.com>
2020-08-18 17:28:54 +0800

    feat: optional结构增加map,flatmap,filter,orelse,orelseget等方法

--- optional/optional.go
+++ optional/optional.go
@@ -15,3 +15,41 @@ func None() Optional {
 func Some(v interface{}) Optional {
 	return Optional{IsNone: false, IsSome: true, Value: v}
 }
+
+func (o Optional) Map(mapper func(value interface{}) interface{}) Optional {
+	if o.IsNone {
+		return o
+	}
+	return Some(mapper(o.Value))
+}
+
+func (o Optional) FlatMap(mapper func(value interface{}) Optional) Optional {
+	if o.IsNone {
+		return o
+	}
+	return mapper(o.Value)
+}
+
+func (o Optional) Filter(predicate func(value interface{}) bool) Optional {
+	if o.IsNone {
+		return o
+	}
+	if predicate(o.Value) {
+		return o
+	}
+	return none
+}
+
+func (o Optional) OrElse(otherValue interface{}) interface{} {
+	if o.IsNone {
+		return otherValue
+	}
+	return o.Value
+}
+
+func (o Optional) OrElseGet(supplier func() interface{}) interface{} {
+	if o.IsNone {
+		return supplier()
+	}
+	return o.Value
+}
--- optional/optional_test.go
+++ optional/optional_test.go
@@ -27,3 +27,54 @@ func TestSome(t *testing.T) {
 	assert.Equal(t, false, b.IsNone)
 	assert.Equal(t, 456, b.Value)
 }
+
+func TestMap(t *testing.T) {
+	assert.Equal(t, Some(123).Map(func(value interface{}) interface{} {
+		assert.Equal(t, value, 123)
+		return 456
+	}), Some(456))
+	assert.Equal(t, None().Map(func(_ interface{}) interface{} {
+		return 456
+	}), None())
+}
+
+func TestFlatMap(t *testing.T) {
+	assert.Equal(t, Some(123).FlatMap(func(value interface{}) Optional {
+		assert.Equal(t, value, 123)
+		return Some(456)
+	}), Some(456))
+	assert.Equal(t, None().FlatMap(func(_ interface{}) Optional {
+		return Some(456)
+	}), None())
+}
+
+func TestOrElse(t *testing.T) {
+	assert.Equal(t, None().OrElse(456), 456)
+	assert.Equal(t, Some(123).OrElse(456), 123)
+}
+
+func TestOrElseGet(t *testing.T) {
+	assert.Equal(t, None().OrElseGet(func() interface{} {
+		return 456
+	}), 456)
+	assert.Equal(t, Some(123).OrElseGet(func() interface{} {
+		return 456
+	}), 123)
+}
+
+func TestFilter(t *testing.T) {
+	assert.Equal(t, None().Filter(func(value interface{}) bool {
+		return true
+	}), None())
+	assert.Equal(t, None().Filter(func(value interface{}) bool {
+		return false
+	}), None())
+	assert.Equal(t, Some(123).Filter(func(value interface{}) bool {
+		assert.Equal(t, value, 123)
+		return true
+	}), Some(123))
+	assert.Equal(t, Some(123).Filter(func(value interface{}) bool {
+		assert.Equal(t, value, 123)
+		return false
+	}), None())
+}
