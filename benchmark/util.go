810db498e77a0ecefad03f372cf92835fcdd0a27
Zongmin Lei <leizongmin@gmail.com>
2020-08-18 18:41:14 +0800

    fix: 增加benchmark.SpentTime()

--- benchmark/util.go
+++ benchmark/util.go
@@ -10,6 +10,6 @@ func SpentTime(msg string, f func()) time.Duration {
 	start := time.Now()
 	f()
 	spent := time.Now().Sub(start)
-	fmt.Printf("[spent:%s]\t%s", spent, msg)
+	fmt.Printf("[spent:%s]\t%s\n", spent, msg)
 	return spent
 }
