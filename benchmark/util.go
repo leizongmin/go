8255246b8bca7362a566e9db77be773272726338
Zongmin Lei <leizongmin@gmail.com>
2020-08-19 11:21:18 +0800

    refactor(benchmark): SpentTime优化输出

--- benchmark/util.go
+++ benchmark/util.go
@@ -1,15 +1,23 @@
 package benchmark
 
 import (
-	"fmt"
+	"log"
+	"os"
 	"time"
 )
 
+var spentTimeLog *log.Logger
+
+func init() {
+	spentTimeLog = log.New(os.Stderr, "[benchmark.SpentTime] ", log.LstdFlags)
+}
+
 // 打印执行函数消耗的时间
 func SpentTime(msg string, f func()) time.Duration {
+	spentTimeLog.Println(msg)
 	start := time.Now()
 	f()
 	spent := time.Now().Sub(start)
-	fmt.Printf("[spent:%s]\t%s\n", spent, msg)
+	spentTimeLog.Printf("%s\t(%s)", msg, spent)
 	return spent
 }
