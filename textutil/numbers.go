f4731d0e9b20bb54997471c062c3b1db9f3ac70a
Zongmin Lei <leizongmin@gmail.com>
2020-08-19 11:09:27 +0800

    feat(textutil): 增加TryParseUint,TryParseUint64,TryParseFloat32,TryParseFloat64,TryParseBool

--- textutil/numbers.go
+++ textutil/numbers.go
@@ -19,5 +19,50 @@ func TryParseInt64(s string, defaultValue int64) int64 {
 	if err != nil {
 		return defaultValue
 	}
-	return int64(v)
+	return v
+}
+
+// 尝试解析uint，如果失败则返回默认值
+func TryParseUint(s string, defaultValue uint) uint {
+	v, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		return defaultValue
+	}
+	return uint(v)
+}
+
+// 尝试解析uint64，如果失败则返回默认值
+func TryParseUint64(s string, defaultValue uint64) uint64 {
+	v, err := strconv.ParseUint(s, 10, 64)
+	if err != nil {
+		return defaultValue
+	}
+	return v
+}
+
+// 尝试解析float32，如果失败则返回默认值
+func TryParseFloat32(s string, defaultValue float32) float32 {
+	v, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		return defaultValue
+	}
+	return float32(v)
+}
+
+// 尝试解析float64，如果失败则返回默认值
+func TryParseFloat64(s string, defaultValue float64) float64 {
+	v, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return defaultValue
+	}
+	return v
+}
+
+// 尝试解析bool，如果失败则返回默认值
+func TryParseBool(s string, defaultValue bool) bool {
+	v, err := strconv.ParseBool(s)
+	if err != nil {
+		return defaultValue
+	}
+	return v
 }
--- textutil/numbers_test.go
+++ textutil/numbers_test.go
@@ -15,3 +15,28 @@ func TestTryParseInt64(t *testing.T) {
 	assert.Equal(t, int64(12345), TryParseInt64("12345", 0))
 	assert.Equal(t, int64(54321), TryParseInt64("aaa", 54321))
 }
+
+func TestTryParseUint(t *testing.T) {
+	assert.Equal(t, uint(12345), TryParseUint("12345", 0))
+	assert.Equal(t, uint(54321), TryParseUint("aaa", 54321))
+}
+
+func TestTryParseUint64(t *testing.T) {
+	assert.Equal(t, uint64(12345), TryParseUint64("12345", 0))
+	assert.Equal(t, uint64(54321), TryParseUint64("aaa", 54321))
+}
+
+func TestTryParseFloat32(t *testing.T) {
+	assert.Equal(t, float32(123.45), TryParseFloat32("123.45", 0))
+	assert.Equal(t, float32(543.21), TryParseFloat32("aaa.bb", 543.21))
+}
+
+func TestTryParseFloat64(t *testing.T) {
+	assert.Equal(t, 123.45, TryParseFloat64("123.45", 0))
+	assert.Equal(t, 543.21, TryParseFloat64("aaa.bb", 543.21))
+}
+
+func TestTryParseBool(t *testing.T) {
+	assert.Equal(t, true, TryParseBool("true", false))
+	assert.Equal(t, false, TryParseBool("x", false))
+}
